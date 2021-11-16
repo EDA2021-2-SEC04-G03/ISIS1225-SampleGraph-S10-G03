16ddcd2fa44d7b923fe9a2f95b5e23d17914e152
Daniela Camacho <d.camachom@uniandes.edu.co>
2021-11-16 09:54:46 -0500

    cambio

--- App/view.py
+++ App/view.py
@@ -45,7 +45,7 @@ operación seleccionada.
 # ___________________________________________________
 
 
-servicefile = 'bus_routes_14000.csv'
+servicefile = 'bus_routes_10000.csv'
 initialStation = None
 
 # ___________________________________________________
